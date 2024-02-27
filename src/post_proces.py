17f72a5bedf8c13d76610901a436754f044ff527
sandernoels <sander.noels@silverfin.com>
2024-02-27 14:46:40 +0000

    language identifier

--- src/post_proces.py
+++ src/post_proces.py
@@ -6,9 +6,11 @@ from functools import partial
 from typing import Any, Dict
 from uuid import uuid4
 
+import fasttext
 from datasets import load_from_disk
+from huggingface_hub import HfApi, hf_hub_download
 from translation_service import call_chatgpt_sync, load_config
-from utils import filter_dataset
+from utils import filter_dataset, identify_language
 
 
 def parse_args() -> argparse.Namespace:
@@ -32,7 +34,7 @@ def update_translation(
     return new_value if idx == idx_to_change else example
 
 
-def main(args: argparse.Namespace):
+def filter_translations(args: argparse.Namespace):
     dataset = load_from_disk(args.db_location)
     config = load_config(args)
 
@@ -40,6 +42,7 @@ def main(args: argparse.Namespace):
     os.makedirs(temporary_location, exist_ok=True)
 
     for dataset_keys, dataset_values in dataset.items():
+        remove_indexes = []
         for idx, row in enumerate(dataset_values):
             row = copy.deepcopy(row)
 
@@ -70,6 +73,75 @@ def main(args: argparse.Namespace):
                 print(
                     f"Row {idx} translation failed after {args.max_retries} attempts."
                 )
+                remove_indexes.append(idx)
+
+        print("to_remove", remove_indexes)
+        dataset[dataset_keys] = dataset_values.filter(
+            lambda _, idx: idx not in remove_indexes, with_indices=True
+        )
+
+    # save the modified dataset to a temporary location
+    dataset.save_to_disk(temporary_location)
+
+    # replace the original dataset with the updated one from temporary location
+    shutil.rmtree(args.db_location)
+    shutil.move(temporary_location, args.db_location)
+
+
+def check_language(args: argparse.Namespace, target_language="nld"):
+    dataset = load_from_disk(args.db_location)
+    config = load_config(args)
+
+    temporary_location = os.path.join(os.path.dirname(args.db_location), str(uuid4()))
+    os.makedirs(temporary_location, exist_ok=True)
+
+    model_path = hf_hub_download(
+        repo_id="facebook/fasttext-language-identification", filename="model.bin"
+    )
+    model = fasttext.load_model(model_path)
+
+    for dataset_keys, dataset_values in dataset.items():
+        remove_indexes = []
+        for idx, row in enumerate(dataset_values):
+            row = copy.deepcopy(row)
+
+            if identify_language(row, "translation", model) == target_language:
+                continue
+
+            for retry in range(args.max_retries):
+                try:
+                    translation = call_chatgpt_sync(config, row["prompt"])
+                    row["translation"] = translation
+                    if (
+                        translation
+                        and identify_language(row, "translation", model)
+                        == target_language
+                    ):
+                        dataset[dataset_keys] = dataset_values.map(
+                            partial(
+                                update_translation, idx_to_change=idx, new_value=row
+                            ),
+                            with_indices=True,
+                        )
+                        print(
+                            f"Row {idx} translation succeeded after {retry + 1} attempts. The translation has been changed to Dutch."
+                        )
+                        break
+
+                except Exception as e:
+                    print(
+                        f"Row {idx} translation failed on attempt {retry + 1}: {str(e)}"
+                    )
+            else:
+                print(
+                    f"Row {idx} translation failed after {args.max_retries} attempts. Couldn't convert to Dutch."
+                )
+                remove_indexes.append(idx)
+
+        print("to_remove", remove_indexes)
+        dataset[dataset_keys] = dataset_values.filter(
+            lambda _, idx: idx not in remove_indexes, with_indices=True
+        )
 
     # save the modified dataset to a temporary location
     dataset.save_to_disk(temporary_location)
@@ -80,4 +152,10 @@ def main(args: argparse.Namespace):
 
 
 if __name__ == "__main__":
-    main(parse_args())
+    args = parse_args()
+
+    # filter translations
+    filter_translations(args)
+
+    # check languages
+    check_language(args)
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,11 @@
 import re
 import unicodedata as ud
+from typing import Optional, Tuple
 
 latin_letters = {}
 
 
-def word_indexes(text: str, lookup: str) -> tuple[int, int]:
+def word_indexes(text: str, lookup: str) -> Optional[Tuple[int, int]]:
     """Will return begin and index of a word to lookup.
 
     Params:
@@ -15,23 +16,10 @@ def word_indexes(text: str, lookup: str) -> tuple[int, int]:
     Raises:
         AttributeError when the word is not found
     """
-    lookupstr = r"\b(" + lookup + r")\b"
-    result = re.search(lookupstr, text)
-    begin = result.start()
-    end = result.end()
-    return begin, end
-
-
-def is_translation_valid(text: str) -> bool:
-    if not text:
-        return False
-    try:
-        word_indexes(text, "instruction: ")
-        word_indexes(text, "input: ")
-        word_indexes(text, "response: ")
-        return True
-    except Exception:
-        return False
+    result = re.search(r"\b(" + lookup + r")\b", text)
+    if result is None:
+        return None
+    return result.start(), result.end()
 
 
 def is_latin(unicode_chr: str):
@@ -41,68 +29,77 @@ def is_latin(unicode_chr: str):
         return latin_letters.setdefault(unicode_chr, "LATIN" in ud.name(unicode_chr))
 
 
-def only_roman_chars(unicode_text: str):
+def only_roman_chars(unicode_text: str) -> bool:
     return all(is_latin(uchr) for uchr in unicode_text if uchr.isalpha())
 
 
-def filter_dataset(row, column_name: str):
-    ai_names = [
-        "AI-assistent",
-        "AI-gebaseerde assistent",
-        "virtuele assistent",
-        "digitale assistent",
-        "tekst-assistent",
-        "AI tekstgebaseerde asssistent",
-        "tekstgebaseerde asssistent",
-        "assistent",
-        "taalmodel",
-        "AI-taalmodel",
-        "AI taalmodel",
-    ]
-    regex_names = re.compile(
-        r"|".join(
-            [
-                rf"als (?:een )?{name}|ben (?:een )?{name}|{name} ben"
-                for name in ai_names
+AI_NAMES_REGEX = re.compile(
+    r"|".join(
+        [
+            rf"als (?:een )?{name}|ben (?:een )?{name}|{name} ben"
+            for name in [
+                "AI-assistent",
+                "AI-gebaseerde assistent",
+                "virtuele assistent",
+                "digitale assistent",
+                "tekst-assistent",
+                "AI tekstgebaseerde asssistent",
+                "tekstgebaseerde asssistent",
+                "assistent",
+                "taalmodel",
+                "AI-taalmodel",
+                "AI taalmodel",
             ]
-        ),
-        flags=re.IGNORECASE,
-    )
+        ]
+    ),
+    flags=re.IGNORECASE,
+)
 
-    brands = [
-        "ChatGPT",
-        "Chat GPT",
-        "GPT3",
-        "GPT 3",
-        "gpt-3",
-        "gpt-3.5-turbo",
-        "GPT4",
-        "GPT 4",
-        "gpt-4",
-        "gpt-4-turbo",
-        "OpenAI",
-        "ShareGPT",
-    ]
-    regex_brands = re.compile(r"|".join(brands), flags=re.IGNORECASE)
+BRANDS_REGEX = re.compile(
+    r"|".join(
+        [
+            "ChatGPT",
+            "Chat GPT",
+            "GPT3",
+            "GPT 3",
+            "gpt-3",
+            "gpt-3.5-turbo",
+            "GPT4",
+            "GPT 4",
+            "gpt-4",
+            "gpt-4-turbo",
+            "OpenAI",
+            "ShareGPT",
+        ]
+    ),
+    flags=re.IGNORECASE,
+)
 
-    knowledge_cut_offs = [
-        "kennisafsluiting in 2023",
-        "kennisstop in 2023",
-        "kennisafsnijdatum van 2023",
-        "cutoff in 2023",
-        "Tot mijn kennis die bijgewerkt is tot begin 2023",
-        "Voor zover mijn kennis reikt tot 2023",
-        "Vanaf mijn kennis tot begin 2023",
-        "As of my last update in 2023",
-    ]
-    regex_knowledge = re.compile(r"|".join(knowledge_cut_offs), flags=re.IGNORECASE)
+KNOWLEDGE_REGEX = re.compile(
+    r"|".join(
+        [
+            "kennisafsluiting in 2023",
+            "kennisstop in 2023",
+            "kennisafsnijdatum van 2023",
+            "cutoff in 2023",
+            "Tot mijn kennis die bijgewerkt is tot begin 2023",
+            "Voor zover mijn kennis reikt tot 2023",
+            "Vanaf mijn kennis tot begin 2023",
+            "As of my last update in 2023",
+        ]
+    ),
+    flags=re.IGNORECASE,
+)
 
-    incorrect_language = ["It seems like there was a typo", "assistant"]
-    regex_language = re.compile(r"|".join(incorrect_language), flags=re.IGNORECASE)
+INCORRECT_LANGUAGE_REGEX = re.compile(
+    r"|".join(["It seems like there was a typo", "assistant"]), flags=re.IGNORECASE
+)
+APOLOGIES_REGEX = re.compile(
+    r"|".join(["spijt me", "spijt mij", "sorry", "mijn excuses"]), flags=re.IGNORECASE
+)
 
-    apologies = ["spijt me", "spijt mij", "sorry", "mijn excuses"]
-    regex_apologies = re.compile(r"|".join(apologies), flags=re.IGNORECASE)
 
+def filter_dataset(row, column_name: str) -> bool:
     text = row[column_name]
     if text is None:
         return False
@@ -110,28 +107,49 @@ def filter_dataset(row, column_name: str):
     text = text.replace("\n", " ")
     text = " ".join(text.split())
 
-    if regex_names.search(text):
+    # check if any of 'ai_names' exist in `text`
+    if AI_NAMES_REGEX.search(text):
+        return False
+    # check if any of 'brands' regex matches exist in `text`
+    if BRANDS_REGEX.search(text):
         return False
 
-    if regex_brands.search(text):
+    # check if any 'knowledge_cut_offs' regex matches exist in `text`
+    if KNOWLEDGE_REGEX.search(text):
         return False
 
-    if regex_knowledge.search(text):
+    # check if any 'incorrect_language' regex matches exist in `text`
+    if INCORRECT_LANGUAGE_REGEX.search(text):
         return False
 
-    if regex_language.search(text):
-        return False
-
-    if regex_apologies.search(text):
+    # check if any 'apologies' regex matches exist in `text`
+    if APOLOGIES_REGEX.search(text):
         return False
 
     if not only_roman_chars(text):
         return False
-
     if len(text.split()) <= 3:
         return False
-
     if not is_translation_valid(text):
         return False
 
     return True
+
+
+def is_translation_valid(text: str) -> bool:
+    return all(
+        word_indexes(text, word) is not None
+        for word in ["instruction: ", "input: ", "response: "]
+    )
+
+
+def identify_language(row, column_name: str, model):
+    text = row[column_name]
+    if text is None:
+        return False
+
+    text = text.replace("\n", " ")
+    text = " ".join(text.split())
+
+    predictions = model.predict(text, k=1)
+    return predictions[0][0].replace("__label__", "").replace("_Latn", "")
