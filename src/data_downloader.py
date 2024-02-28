7034f970d4ecc235d6b8cc02dc965d8379991d2e
jorne.deblaere <jorne.deblaere@silverfin.com>
2024-02-28 12:55:13 +0000

    Cleaned data downloader file

--- src/data_downloader.py
+++ src/data_downloader.py
@@ -1,7 +1,10 @@
+"""Download script that will download all the datasets specified in the config file and will save them in the provided location.
+
+"""
 import argparse
 import os
-from configparser import ConfigParser
 
+from config import load_config
 from datasets import load_dataset
 
 
@@ -14,34 +17,29 @@ def load_args():
     return parser.parse_args()
 
 
-def load_config():
-    """Loads configuration from config.ini file."""
-    config = ConfigParser()
-    config.read("./src/config.ini")
-    return config
-
-
-def download_datasets(config, db_location):
-    """Downloads datasets mentioned in config.ini and save under ./data directory"""
-    print(dict(config["DEFAULT"]))
-    dataset_links = config.get("DATASETS", "dataset_links").split(",")
-
-    for dataset_link in dataset_links:
-        dataset_link = dataset_link.strip()
+def download_datasets(datasets: list[str], db_location: str) -> None:
+    """Download the provided datasets and places them in the db_location."""
+    for dataset_link in datasets:
         print(f"Downloading {dataset_link}...")
-        try:
-            dataset = load_dataset(dataset_link)
-            save_path = os.path.join(db_location, dataset_link)
-            dataset.save_to_disk(save_path)
-            print(f"Saved {dataset_link} to {save_path}")
-        except Exception as e:
-            print(f"Failed to download {dataset_link} due to {str(e)}")
+        _download_dataset(dataset_link, db_location)
+
+
+def _download_dataset(dataset_link: str, db_location: str) -> None:
+    dataset_link = dataset_link.strip()
+    try:
+        dataset = load_dataset(dataset_link)
+        save_path = os.path.join(db_location, dataset_link)
+        dataset.save_to_disk(save_path)
+        print(f"Saved {dataset_link} to {save_path}")
+    except Exception as e:
+        print(f"Failed to download {dataset_link} due to {str(e)}")
 
 
 def main():
     args = load_args()
     config = load_config()
-    download_datasets(config, args.db_location)
+    datasets = config["dataset_links"].split(",")
+    download_datasets(datasets, args.db_location)
 
 
 if __name__ == "__main__":
