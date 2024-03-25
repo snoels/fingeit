8ba0267c772b19b53718a6514dddc78b4f964570
jorne.deblaere <jorne.deblaere@silverfin.com>
2024-03-25 07:56:55 +0100

    Added evaluation framework allong with headline, sentiment and convfinqa

---
+++ src/evaluation/README.md
@@ -0,0 +1,44 @@
+# Evaluation package
+
+To evaluate the model a set of metrics are defined per task.
+the tasks can be grouped per dataset so every dataset is evaluated separatly.
+
+## How to use the metrics
+To use the metrics instantiate the class and call the evaluate method. 
+eg.
+
+```python
+evaluator = HeadlineEvaluator(model_path='models/fingeitje')
+evaluation = evaluator.evaluate(dataset_name='ice-hands/finred-messages', split='test')
+print(evaluation)
+```
+
+To gain more info about the evaluation you can extract the processed pandas dataframe from the evaluation class
+
+```python
+df_to_investigate = evaluation.df
+```
+
+### Crashes
+If for some reason the process fails not everything is lost. To mitigate system failures all predictions are stored in a text file. The path will be printed whenever the process has crashed. Recovery method:
+
+
+``` python
+evaluator = HeadlineEvaluator(model_path='models/fingeitje', temp_storage_file=<PATH_TO_THE_EXISTING_FILE>)
+evaluation = evaluator.evaluate(dataset_name='ice-hands/finred-messages', split='test')
+print(evaluation)
+```
+
+The base class will determin what the start index will be and continue from there on. It will also leave a mark where it is continued for investigation purpose later on.
+Notice that the marks will be deleted before adding to the set
+
+
+## How to create your own metrics
+To create your own metrics you can relie on the [evaluator_base](./evaluator_base.py) module.
+The module holds a baseclass called `BaseEvaluator`. The base class relies on the template method design pattern that provides a skelleton to create predictions based on a provided dataset and lets the superclass overwrite the `_evaluate` function for specific dataset evaluations.
+
+what you need to do.
+1. Create a new class
+2. Extend the BaseEvaluator class
+3. Add a function called `_evaluate` with defined as: `def _evaluate(self, dataset:Dataset) -> Evaluation:`
+4. Implement the _evaluate function with your custom metric.
---
+++ src/evaluation/evaluator_base.py
@@ -0,0 +1,130 @@
+from dataclasses import dataclass
+from datasets import load_dataset, Dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import torch
+import pandas as pd
+from scripts.evaluation.file_access import FileAccess
+
+@dataclass
+class Metric:
+    name: str
+    value: float
+
+@dataclass
+class Evaluation:
+    df: pd.DataFrame
+    metrics: list[Metric]
+
+    def __str__(self) -> str:
+        metrics_str = "\n".join([f"{x.name}: {x.value}" for x in self.metrics])
+        return f"Scoring metrics: \n {metrics_str}"
+
+class BaseEvaluator:
+    PREDICTION_COLUMN_NAME = 'prediction'
+    GROUND_TRUTH_COLUMN_NAME = 'output'
+
+    def __init__(self, model_path:str, remove_temp_file:bool|None=None, temp_storage_file:str|None=None) -> None:
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        self.model_path = model_path
+        self._log_interval = 20
+        self.file_access = FileAccess(temp_storage_file)
+        self.remove_temp_file = remove_temp_file if remove_temp_file else False
+
+    def _load_dataset(self, dataset_name:str, split:str) -> Dataset:
+        return load_dataset(dataset_name, revision='main')[split]
+
+
+    def _maybe_insert_system_message(self, messages):
+        if messages[0]["role"] == "system":
+            return
+
+        # chat template can be one of two attributes, we check in orders
+        chat_template = self.tokenizer.chat_template
+        if chat_template is None:
+            chat_template = self.tokenizer.default_chat_template
+
+        # confirm the jinja template refers to a system message before inserting
+        if "system" in chat_template or "<|im_start|>" in chat_template:
+            messages.i
+
+
+    def _apply_chat_template(
+        self,
+        example,
+    ):
+        messages = example["messages"][:-1] #Remove the GT answer
+        self._maybe_insert_system_message(messages)
+        example["text"] = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
+        return example
+
+    def _preprocess_dataset(self, dataset:Dataset) -> Dataset:
+        return dataset.map(
+            self._apply_chat_template,
+            num_proc=1,
+            desc="Applying chat template"
+            )
+
+    def _get_response(self, response) -> str:
+        lines = [self.tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in response]
+        assistant = ''.join(lines).split('<|assistant|>')[-1]
+        return assistant.replace('\n', '')
+
+    def _get_start_index(self):
+        return len(self.file_access.read())
+
+    def _to_dataloader(self, dataset:Dataset) -> DataLoader:
+        def collate_fn(batch):
+            inputs = self.tokenizer(
+                [f["text"] for f in batch], return_tensors='pt',
+                padding=True,
+                return_token_type_ids=False
+            )
+            return inputs
+
+        if (start_index := self._get_start_index()) != 0:
+            self.file_access.mark_recovery()
+            dataset = dataset.select(range(start_index,dataset.num_rows))
+
+        return DataLoader(dataset, batch_size=10, collate_fn=collate_fn, shuffle=False)
+
+    def _get_predictions(self, dataset:Dataset) -> Dataset:
+        model = AutoModelForCausalLM.from_pretrained(self.model_path,  device_map="cuda") #do not move this to the constructor, out of memory
+        for idx, inputs in enumerate(tqdm(self._to_dataloader(dataset))):
+            inputs = {key: value.to(model.device) for key, value in inputs.items()}
+            res = model.generate(**inputs, max_length=2048, eos_token_id=self.tokenizer.eos_token_id)
+            res_sentences = [self._get_response(i) for i in res]
+            if (idx + 1) % self._log_interval == 0:
+                tqdm.write(f'{idx}: {res_sentences[0]}')
+            self.file_access.store(res_sentences)
+
+            del res
+            del inputs
+            torch.cuda.empty_cache()
+        
+        return self.file_access.read()
+
+    def _add_predictions(self, dataset:Dataset) -> Dataset:
+        try:
+            predictions = self._get_predictions(dataset)
+            dataset =  dataset.add_column(self.PREDICTION_COLUMN_NAME, predictions)
+            if self.remove_temp_file:
+                self.file_access.remove()
+            return dataset
+        except Exception as e:
+            print(f"woepsie, somehting happend during prediction time. You can view already processed data in the temp file: {self.file_access.file}", e)
+
+    def _evaluate(self, dataset:Dataset) -> Evaluation:
+        ...
+
+
+    def evaluate(self, dataset_name:str, split:str) -> Evaluation:
+        """
+        Params:
+            dataset_name: name of the dataset, can be a huggingface repo or folder
+        """
+        dataset = self._load_dataset(dataset_name, split)
+        dataset = self._preprocess_dataset(dataset)
+        dataset = self._add_predictions(dataset)
+        return self._evaluate(dataset)
